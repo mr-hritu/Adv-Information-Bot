8927c05f06f46507c37871ae0c9ca419c9d9e0b5
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-06-11 12:17:37 +0530

    Update Config.py

--- MT_ID_Bot/Config.py
+++ MT_ID_Bot/Config.py
@@ -2,16 +2,16 @@ import os
 
 class Config(object):
      
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6130457410:AAG8jDlWwyFQv1-u8XYnpDhtXaliPo3YrEY")
 
 
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 29616312))
 
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "dd1a05ab4c47a5a037cc067cf4bded27")
 
 
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "mo_tech_YT")
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Private_Bots")
 
 
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "mt_id_Bot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Prime_Hritu0_bot")
